703b35d36b1c380ec813f559d77559f54fce1d40
mazuh <marcell-mz@hotmail.com>
2023-05-10 13:14:46 -0300

    add some webrtc types

--- src/webrtc/index.ts
+++ src/webrtc/index.ts
@@ -3,6 +3,26 @@ export interface User extends UniqueEntity {
   email: string;
 }
 
+export interface Call extends UniqueEntity {
+  displayName: string;
+  hostId: string;
+  hostDisplayName: string;
+  participantsUids: string[];
+}
+
+export interface CallUser extends UniqueEntity {
+  userUid: string;
+  userDisplayName: string;
+  joined?: number;
+}
+
+export interface CallUserP2PDescription extends UniqueEntity {
+  newestParticipantUid: string;
+  newestParticipantOfferSDP: string;
+  oldestParticipantUid: string;
+  oldestParticipantAnswerSDP: string;
+}
+
 interface UniqueEntity {
   uid: string;
 }
