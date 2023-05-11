f9e876f19d367077a579875e050223d146d4d3e3
Muniz <rcmuniz51@gmail.com>
2023-05-11 00:30:16 -0300

    feat: block login from users without email

--- src/services/firestore-auth.ts
+++ src/services/firestore-auth.ts
@@ -10,14 +10,21 @@ export default firestoreAuth;
 
 async function login() {
   try {
-    const result = await signInWithPopup(getAuth(), googleAuthProvider);
+    const auth = getAuth();
+    const result = await signInWithPopup(auth, googleAuthProvider);
     const {
       user: { uid, displayName, email },
     } = result;
+
+    if (!email) {
+      signOut(auth);
+      throw new Error("Login blocked: unidentified user");
+    }
+
     return {
       uid,
-      displayName: displayName ?? email ?? "unidentified user",
-      email: email ?? "",
+      displayName: displayName ?? email,
+      email,
     };
   } catch (error) {
     throw new Error("Login error.");
