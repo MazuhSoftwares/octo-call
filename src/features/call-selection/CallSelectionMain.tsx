f7ba92c59f5eabc64a3fd3904fe1a25bedba4852
Muniz <rcmuniz51@gmail.com>
2023-05-16 00:16:51 -0300

    create calls slice

--- src/features/call-selection/CallSelectionMain.tsx
+++ src/features/call-selection/CallSelectionMain.tsx
@@ -1,10 +1,40 @@
 import Typography from "@mui/material/Typography";
 import InitialMainCard from "../../components/templates/InitialMainCard";
+import { useRef } from "react";
+import { useAppDispatch, useAppSelector } from "../../state";
+import { createCall } from "../../state/call";
+import { selectCurrentUser } from "../../state/user";
 
 export default function CallSelectionMain() {
+  const displayNameInputRef = useRef<HTMLInputElement>(null);
+  const user = useAppSelector(selectCurrentUser);
+  const dispatch = useAppDispatch();
+
+  const createNewCall = () => {
+    const displayName = displayNameInputRef.current?.value;
+    if (!displayName) {
+      return;
+    }
+
+    dispatch(
+      createCall({
+        hostId: user.uid,
+        hostDisplayName: user.displayName,
+        displayName: displayName,
+      })
+    );
+  };
+
   return (
     <InitialMainCard subtitle="Create or join a call">
       <Typography>Imagine a creation or join form here.</Typography>
+      <label htmlFor="create-call-input">Call Name</label>
+      <input
+        id="create-call-input"
+        name="displayName"
+        ref={displayNameInputRef}
+      />
+      <button onClick={createNewCall}>Create</button>
     </InitialMainCard>
   );
 }
