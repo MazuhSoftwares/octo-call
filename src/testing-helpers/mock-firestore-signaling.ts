a5ab44d5501a67405c38053285b43d17cebd55c6
Muniz <rcmuniz51@gmail.com>
2023-05-30 23:34:04 -0300

    fix createCall mock

--- src/testing-helpers/mock-firestore-signaling.ts
+++ src/testing-helpers/mock-firestore-signaling.ts
@@ -7,13 +7,11 @@ jest.mock("../services/firestore-signaling.ts", () => ({
     participantsUids: ["5g6h7j"],
   }),
   createCall: jest.fn().mockResolvedValue({
-    call: {
-      uid: "1a2b3c",
-      displayName: "Daily",
-      hostId: "5g6h7j",
-      hostDisplayName: "John Doe",
-      participantsUids: ["5g6h7j"],
-    },
+    uid: "1a2b3c",
+    displayName: "Daily",
+    hostId: "5g6h7j",
+    hostDisplayName: "John Doe",
+    participantsUids: ["5g6h7j"],
   }),
   listenCallUsers: jest.fn().mockReturnValue(jest.fn()),
 }));
