c94c5e0a5ce088f960da2d92ce49fc201de3846f
mazuh <marcell-mz@hotmail.com>
2023-06-04 18:19:08 -0300

    fix tests

--- src/features/p2p-call/P2PCallMain.test.tsx
+++ src/features/p2p-call/P2PCallMain.test.tsx
@@ -1,8 +1,31 @@
+import { waitFor } from "@testing-library/react";
 import fullRender from "../../testing-helpers/fullRender";
+import webrtc from "../../webrtc";
+import type { P2PCallConnectionOptions } from "../../webrtc";
 import P2PCallMain from "./P2PCallMain";
+
+jest.mock("../../webrtc", () => ({
+  makeP2PCallConnection: jest.fn((options: P2PCallConnectionOptions) => ({
+    uuid: "call-uuid-123",
+    options,
+    start: jest.fn(),
+    stop: jest.fn(),
+    incomingSignaling: {
+      handleRemoteJsepAction: () => Promise.resolve(),
+      handleRemoteIceCandidate: () => Promise.resolve(),
+    },
+    onLocalStream: options.onLocalStream || null,
+    onRemoteStream: options.onRemoteStream || null,
+  })),
+}));
 
 describe("P2PCallMain", () => {
   it("renders", () => {
     fullRender(<P2PCallMain />);
+  });
+
+  it("starts calls", async () => {
+    fullRender(<P2PCallMain />);
+    await waitFor(() => expect(webrtc.makeP2PCallConnection).toBeCalled());
   });
 });
