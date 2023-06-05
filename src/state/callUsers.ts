d2a0e3a3576c56917bc8f6f5b2c8a96c811403ff
Muniz <rcmuniz51@gmail.com>
2023-06-05 02:00:16 -0300

    remove unneeded import

--- src/state/callUsers.ts
+++ src/state/callUsers.ts
@@ -1,4 +1,4 @@
-import { PayloadAction, createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { PayloadAction, createSlice } from "@reduxjs/toolkit";
 import { RootState } from ".";
 import { CallUser } from "../webrtc";
 
