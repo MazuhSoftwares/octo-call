720a3b9caadd1e37a49aad4177c6122a75631c8e
Muniz <rcmuniz51@gmail.com>
2023-06-06 23:06:15 -0300

    create join call test

--- src/features/call-selection/CallCreationMain.test.tsx
+++ src/features/call-selection/CallCreationMain.test.tsx
@@ -95,4 +95,34 @@ describe("CallCreationMain", () => {
       )
     ).toBeVisible();
   });
+
+  it("asking to join a call is integrated with firebase", async () => {
+    fullRender(<CallSelectionMain />, {
+      preloadedState: {
+        user: {
+          ...userInitialState,
+          uid: "1m2kkn3",
+          displayName: "John Doe",
+          status: "authenticated",
+        },
+      },
+    });
+
+    const callUidInputElement = screen.getByLabelText("Join a call");
+    fireEvent.change(callUidInputElement, {
+      target: { value: "b385c5fe-5da5-476d-b66f-a4580581be61" },
+    });
+
+    const joinCallButtonElement = screen.getByRole("button", {
+      name: "Join call",
+    });
+    await act(() => fireEvent.click(joinCallButtonElement));
+
+    expect(firestoreSignaling.askToJoinCall).toHaveBeenCalledTimes(1);
+    expect(firestoreSignaling.askToJoinCall).toHaveBeenCalledWith({
+      callUid: "b385c5fe-5da5-476d-b66f-a4580581be61",
+      userUid: "1m2kkn3",
+      userDisplayName: "John Doe",
+    });
+  });
 });
