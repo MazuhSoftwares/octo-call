f205bde8a18e1b3addf1890fbab8eed82fc69a52
Muniz <rcmuniz51@gmail.com>
2023-10-26 23:03:56 -0300

    tests: fix tests

--- src/services/firestore-auth.test.ts
+++ src/services/firestore-auth.test.ts
@@ -1,17 +1,34 @@
 import "../testing-helpers/mock-firebase-auth";
 import firestoreAuth from "./firestore-auth";
 import * as firebaseAuth from "firebase/auth";
+import { doc, setDoc } from "firebase/firestore";
+import { v4 as uuidv4 } from "uuid";
+
+jest.mock("firebase/firestore", () => ({
+  doc: jest.fn(),
+  setDoc: jest.fn(),
+}));
+
+jest.mock("uuid", () => ({
+  v4: jest.fn(),
+}));
 
 describe("firestoreAuth", () => {
+  beforeEach(() => {
+    (uuidv4 as jest.Mock).mockReturnValueOnce("7390-4d50-91f3-abcdef1e0d50");
+  });
   it("should successfully log in and return user information", async () => {
     jest.spyOn(firebaseAuth, "getAuth");
     jest.spyOn(firebaseAuth, "getRedirectResult");
+    (doc as jest.Mock).mockImplementationOnce((_, path) => path);
+    (setDoc as jest.Mock).mockResolvedValueOnce(Promise.resolve(null));
     await firestoreAuth.login();
     const user = await firestoreAuth.loadUser();
     expect(user).toEqual({
       uid: "abc123def456",
       displayName: "Jane Doe",
       email: "jane@example.com",
+      deviceUuid: "7390-4d50-91f3-abcdef1e0d50",
     });
   });
 
